be52d700df174749ecab96deaefdbd2212bf9707
Yury Kinchin <yury.kinchyn@gmail.com>
2024-06-05 21:14:56 +0300

    feat: setup project(husky,eslint,prettier)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			'@components': path.resolve(__dirname, './src/components'),
+			'@utils': path.resolve(__dirname, './src/utils'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@constants': path.resolve(__dirname, './src/constants')
+		}
+	}
 })
