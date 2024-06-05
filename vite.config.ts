aeeb9f9ba8061c7bef8ef4a2e3304b97dbc329e5
Yury Kinchin <yury.kinchyn@gmail.com>
2024-06-05 23:57:29 +0300

    feat: add logo asset

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
 			'@components': path.resolve(__dirname, './src/components'),
 			'@utils': path.resolve(__dirname, './src/utils'),
 			'@pages': path.resolve(__dirname, './src/pages'),
-			'@constants': path.resolve(__dirname, './src/constants')
+			'@constants': path.resolve(__dirname, './src/constants'),
+			'@assets': path.resolve(__dirname, './src/assets')
 		}
 	}
 })
