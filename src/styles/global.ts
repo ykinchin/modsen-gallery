c125c667200c182dd19a9733848b46636857f68d
Yury Kinchin <yury.kinchyn@gmail.com>
2024-06-10 03:21:50 +0300

    feat: add global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,16 +2,21 @@ import { createGlobalStyle } from 'styled-components'
 import { baseTheme } from './theme'
 
 export const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lexend+Deca:wght@100..900&display=swap');
+
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
 
-    
 }
 
 html,body {
     height: 100%;
+}
+
+body{
+    font-family: ${baseTheme.fonts.secondary};
 }
 
 a {
@@ -21,5 +26,21 @@ a {
     &:hover {
         color: ${baseTheme.colors.higlight}
     }
+}
+
+input {
+    /* width: 100%;
+    height: 100%;
+    padding: 16px;
+    border-radius: 16px;
+    background: rgba(57, 57, 57, 0.05);
+    border: none; */
+
+    width: 100%;
+    padding: 16px;
+    border-radius: 16px;
+    background: rgba(57, 57, 57, 0.05);
+    border: none;
+    outline: none;
 }
 `
